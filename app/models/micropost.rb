60e738bde0614c83a058b0062e5668f9ec44d01d
hayato.saishu <h3.sbskll9@gmail.com>
2020-10-30 07:52:59 +0000

    foreign_key before

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,13 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  #has_many :favorites, foreign_key: 'micropost_id'
+  #has_many :users,through: :favorites
   
   validates :content, presence: true, length: {maximum: 255}
+  
+  
+  #has_many :favorites, foreign_key: :micropost_id
+  #has_many :favoers, trough: :favorites, source: :user
+  #has_many :favorites
+
 end
